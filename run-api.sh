d637c0eaa38e06c01a9342487bf72f492182fe91
Uli <uli.hitzel@gmail.com>
2018-11-08 14:50:51 +0700

    Create run-api.sh

---
+++ run-api.sh
@@ -0,0 +1,5 @@
+/opt/cassandra/bin/cassandra -R
+
+/opt/Axway-7.6.0/apigateway/posix/bin/nodemanager -d
+
+/opt/Axway-7.6.0/apigateway/posix/bin/startinstance -g "QuickStart Group" -n "QuickStart Server" -d
